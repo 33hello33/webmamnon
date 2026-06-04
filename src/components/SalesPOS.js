9617c07b2ec8454cc49632764a8bd50dda6166c1
33hello33 <pqviet94@gmail.com>
2026-06-04 13:09:22 +0700

    fix lỗi xem ảnh ck

--- src/components/SalesPOS.js
+++ src/components/SalesPOS.js
@@ -601,7 +601,7 @@ export default function SalesPOS() {
                   </div>
 
                   <div style={{ marginTop: "30px", textAlign: "center", fontStyle: "italic", borderTop: '1px dashed #ccc', paddingTop: '10px', fontSize: '10pt' }}>
-                     Facebook: {config ? tencongty || 'Tên Công ty'}
+                     Facebook: {config?.tencongty || 'Tên Công ty'}
                   </div>
                </div>
             </div>
